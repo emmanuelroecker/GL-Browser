2ffeed37934a2f6f4a1e73ade08a48914d155a15
Emmanuel ROECKER <emmanuel.roecker@glicer.com>
2016-01-07 23:01:33 +0100

    article2

---
+++ inject.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function modify() {
+    let cnso = document.getElementById("cnso");
+    if (cnso) {
+        cnso.remove();
+    }
+    let cnsh = document.getElementById("cnsh");
+    if (cnsh) {
+        cnsh.remove();
+    }
+}
+
+let MutationObserver = window.MutationObserver;
+if (MutationObserver) {
+    let observer = new MutationObserver(function(mutations) {
+        modify();
+    });
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+}
+
