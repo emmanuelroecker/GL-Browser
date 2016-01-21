7562d8f674e7030d28aff718fa6223595463b344
Emmanuel ROECKER <emmanuel.roecker@glicer.com>
2016-01-21 14:35:41 +0100

    lint

--- main.js
+++ main.js
@@ -2,30 +2,38 @@
 const electron = require('electron');
 const yaml = require('js-yaml');
 const fs = require('fs');
+const path = require('path');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow;
 
-app.on('window-all-closed', function() {
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
+app.on('window-all-closed', function () {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
 });
 
-app.on('ready', function() {
-  console.log("Electron Version : " + process.versions.electron);
+app.on('ready', function () {
+	console.log('Electron Version : ' + process.versions.electron);
 
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600
+	});
+	mainWindow.loadURL(path.join('file://', __dirname, '/index.html'));
 
-  let filter = yaml.safeLoad(fs.readFileSync('filter.yml', 'utf8'));
-  let ses = mainWindow.webContents.session;
-  ses.webRequest.onBeforeRequest({urls: filter}, function(details, callback) {
-      callback({cancel : true});
-  });
+	let filter = yaml.safeLoad(fs.readFileSync('filter.yml', 'utf8'));
+	let ses = mainWindow.webContents.session;
+	ses.webRequest.onBeforeRequest({
+		urls: filter
+	}, function (details, callback) {
+		callback({
+			cancel: true
+		});
+	});
 
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
+	mainWindow.on('closed', function () {
+		mainWindow = null;
+	});
 });
