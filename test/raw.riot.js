22e258c462c92a8e9495db93b913efb668e27850
Emmanuel ROECKER <emmanuel.roecker@glicer.com>
2016-04-12 16:44:43 +0200

    add raw riot test

---
+++ test/raw.riot.js
@@ -0,0 +1,57 @@
+/*
+Copyright (C) 2016
+Emmanuel ROECKER and Rym BOUCHAGOUR
+http://dev.glicer.com
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+/* global describe, it, document */
+
+'use strict';
+
+const assert = require('assert');
+const fs = require('fs');
+const riot = require('riot');
+
+describe('raw riot', function () {
+	it('compile', function () {
+		let rawTag = fs.readFileSync('./components/UX/raw.riot.tag', 'utf8');
+		assert.equal('raw', eval(riot.compile(rawTag)));
+	});
+	it('mount', function () {
+		let rawTag = fs.readFileSync('./components/UX/raw.riot.tag', 'utf8');
+		eval(riot.compile(rawTag));
+		let html = document.createElement('raw');
+		document.body.appendChild(html);
+		let tag = riot.mount('raw')[0];
+		assert.equal(true, tag.isMounted);
+	});
+	it('without raw', function () {
+		let html = document.createElement('div');
+		html.textContent = '<p><b>test</b></p>';
+		document.body.appendChild(html);
+		assert.equal('<p><b>test</b></p>',document.querySelector('div').textContent);
+	});
+	it('with raw', function () {
+		let rawTag = fs.readFileSync('./components/UX/raw.riot.tag', 'utf8');
+		eval(riot.compile(rawTag));
+		let html = document.createElement('raw');
+		document.body.appendChild(html);
+		riot.mount('raw', {
+			content: '<div><b>test</b></div>'
+		})[0];
+		assert.equal('test', document.querySelector('raw').textContent);
+	});
+});
