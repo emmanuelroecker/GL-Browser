7385eff1b03d86bcbba5d68a5d4c0ffb17992e17
Emmanuel ROECKER <emmanuel.roecker@glicer.com>
2016-05-02 15:47:43 +0200

    add cov report

---
+++ tests/coverage.js
@@ -0,0 +1,29 @@
+/* global __coverage__ */
+
+'use strict';
+
+const path = require('path');
+const istanbul = require('istanbul');
+const mocha = require('mocha');
+const util = require('util');
+
+// Override __src to point to instrumented sources!
+global.__src = path.resolve(__dirname, '..', 'js-cov');
+
+module.exports = Cover;
+
+function Cover(runner) {
+	mocha.reporters.Dot.call(this, runner);
+
+	runner.on('end', function () {
+		let reporter = new istanbul.Reporter();
+		let collector = new istanbul.Collector();
+
+		collector.add(__coverage__);
+
+		reporter.addAll(['text-summary', 'json']);
+		reporter.write(collector, true, () => {});
+	});
+}
+
+util.inherits(Cover, mocha.reporters.Dot);
